b3704479a74796867b09511ef0fa101126ac3557
g-i-kala <5star@wp.pl>
2025-09-07 19:50:17 +0200

    refactor: add DashboardController, relocate rename dashboard blade view

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('dashboard.index');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::get('/test', function () {
