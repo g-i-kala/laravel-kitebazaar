9880bedf3f82ddb04a52b44ff9a95531cd688634
g-i-kala <5star@wp.pl>
2025-09-14 11:37:44 +0200

    feat: visual recognition update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -43,8 +43,7 @@
         class="flex items-center justify-center w-full transition-opacity opacity-100 duration-750 lg:grow starting:opacity-0">
         <main class="flex max-w-[335px] w-full flex-col-reverse lg:max-w-4xl lg:flex-row">
             <div class="text-2xl font-bold">
-                <h1>
-                    KiteBazaar</h1>
+                <img src="{{ Vite::asset('resources/images/kiteBazaar_complete.png') }}" alt="KiteBazaar" />
             </div>
         </main>
     </div>
